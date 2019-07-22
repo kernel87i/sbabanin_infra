7aa80f2e7ea9013e5c5666f9116e299036189d4b
root <root@localhost.localdomain>
2019-07-22 17:06:18 -0400

    adedd terraform

---
+++ terraform/files/outputs.tf
@@ -0,0 +1,3 @@
+output "app_external_ip" {
+  value = "34.68.71.223"
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,57 +1,59 @@
 provider "google" {
-  project = "infra-244920"
-  version = "2.0.0"
-  region = "europe-west-1"
-  zone = "europe-west1-b"
+  project     = "${var.project}"
+  version     = "2.0.0"
+  region      = "${var.region}"
+  zone        = "${var.zone}"
   credentials = "${file("/tmp/terraform/infra-56cf0aa341c8.json")}"
 }
+
 resource "google_compute_instance" "app" {
-  name = "reddit-app"
+  name         = "reddit-app"
   machine_type = "g1-small"
-  zone = "us-central1-a"
+  zone         = "${var.zone}"
 
-metadata {
-  ssh-keys = "appuser:${file("~/.ssh/appuser.pub")}"
-}
-
-boot_disk {
-  initialize_params {
-    image = "reddit-base"
+  metadata {
+    ssh-keys = "appuser:${file(var.public_key_path)}"
   }
- }
 
-tags = ["reddit-app"]
+  boot_disk {
+    initialize_params {
+      image = "${var.disk_image}"
+    }
+  }
+
+  tags = ["reddit-app"]
+
   network_interface {
-  network = "default"
-  access_config {}
-}
+    network       = "default"
+    access_config = {}
+  }
 
-connection {
-  type = "ssh"
-  user = "appuser"
-  agent = false
-  private_key = "${file("~/.ssh/appuser")}"
-}
+  connection {
+    type        = "ssh"
+    user        = "appuser"
+    agent       = false
+    private_key = "${file(var.private_key_path)}"
+  }
 
-provisioner "file" {
-  source = "/tmp/terraform/files/puma.service"
-  destination = "/tmp/puma.service"
-}
+  provisioner "file" {
+    source      = "/tmp/terraform/files/puma.service"
+    destination = "/tmp/puma.service"
+  }
 
-provisioner "remote-exec" {
-   script = "/tmp/terraform/files/deploy.sh"
-}
+  provisioner "remote-exec" {
+    script = "/tmp/terraform/files/deploy.sh"
+  }
 }
 
 resource "google_compute_firewall" "firewall_puma" {
-  name = "allow-puma-default"
+  name    = "allow-puma-default"
   network = "default"
- allow {
-  protocol = "tcp"
-  ports = ["9292"]
-}
 
- source_ranges = ["0.0.0.0/0"]
- target_tags = ["reddit-app"]
-}
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
 
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["reddit-app"]
+}
